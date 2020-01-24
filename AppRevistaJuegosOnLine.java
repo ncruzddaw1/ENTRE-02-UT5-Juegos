227d42728c0426ae439ec0106c4c7714ec1189e3
Nicolas Cruz Diaz <ncruzddaw1@gmail.com>
2020-01-24 21:09:34 +0100

    Terminado proyecto 2 UT5 Juegos

--- AppRevistaJuegosOnLine.java
+++ AppRevistaJuegosOnLine.java
@@ -1,15 +1,23 @@
 /**
  * Punto de entrada a la aplicación
  * 
- * @author - 
+ * @Nicolas Cruz Diaz
  */
 public class AppRevistaJuegosOnLine 
 {
-	public static void main(String[] args)
-	{
-		
-
-		 
-	}
-
+    public static void main(String[] args)
+    {       
+        if(args.length == 0)
+         {
+             System.out.println("Error de sistaxis, java AppRevistaJuegosOnLine <nom> <n1>" + 
+                                "------------------------------------");
+         }
+         else
+         {
+            String nom = args[0];
+            int n1 = Integer.parseInt(args[1]);
+            RevistaOnLineJuegos revista = new RevistaOnLineJuegos(nom, n1);
+            revista.leerDeFichero();
+         }        
+    }
 }
--- RevistaOnLineJuegos.java
+++ RevistaOnLineJuegos.java
@@ -1,7 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
-
+import java.util.Arrays;
 /**
  * La clase representa a una tienda on-line en la
  * que se publican los juegos que se van lanzando al mercado
@@ -12,127 +12,159 @@ import java.util.Scanner;
  */
 public class RevistaOnLineJuegos 
 {
-	private String nombre;
-	private Juego[] juegos;
-	private int total;
+    private String nombre;
+    private Juego[] juegos;
+    private int total;
 
-	/**
-	 * Constructor  
-	 * Crea el array de juegos al tamaño máximo indicado por la constante
-	 * e inicializa el resto de atributos
-	 */
-	public RevistaOnLineJuegos(String nombre, int n) {
-		 
-	}
+    /**
+     * Constructor  
+     * Crea el array de juegos al tamaño máximo indicado por la constante
+     * e inicializa el resto de atributos
+     */
+    public RevistaOnLineJuegos(String nombre, int n)
+    {
+        this.nombre = nombre;
+        juegos = new Juego[n];
+        total = 0;
+    }
 
-	/**
-	 * Devuelve true si el array está completo, false en otro caso
-	 */
-	public boolean estaCompleta() {
-		 
-	}
+    /**
+       * Devuelve true si el array está completo, false en otro caso
+     */
+    public boolean estaCompleta() 
+    {
+         return total == juegos.length;
+    }
 
-	/**
-	 *    Añade un nuevo juego solo si el array no está completo y no existe otro juego
-	 *    ya con el mismo nombre.  Si no se puede añadir se muestra los mensajes adecuados 
-	 *    (diferentes en cada caso)
-	 *    
-	 *    El juego se añade de tal forma que queda insertado en orden alfabético de título
-	 *    (de menor a mayor)
-	 *     !!OJO!! No hay que ordenar ni utilizar ningún algoritmo de ordenación
-	 *    Hay que insertar en orden 
-	 *    
-	 */
-	public void add(Juego juego) {
-		 
+    /**
+     *    Añade un nuevo juego solo si el array no está completo y no existe otro juego
+     *    ya con el mismo nombre.  Si no se puede añadir se muestra los mensajes adecuados 
+     *    (diferentes en cada caso)
+     *    
+     *    El juego se añade de tal forma que queda insertado en orden alfabético de título
+     *    (de menor a mayor)
+     *     !!OJO!! No hay que ordenar ni utilizar ningún algoritmo de ordenación
+     *    Hay que insertar en orden 
+     *    
+     */
+    public void add(Juego juego) 
+    {
+         
 
-	}
+    }
 
-	 
+     
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y
+     */
+    public boolean estaCompleta()
+    {
+        return total == juegos.length;
+    }
 
-	/**
-	 * Efectúa una búsqueda en el array del juego cuyo titulo se
-	 * recibe como parámetro. Es ndiferente mayúsculas y minúsculas
-	 * Si existe el juego devuelve su posición, si no existe devuelve -1
-	 */
-	public int existeJuego(String titulo) {
-		 
-	    
-		return 0;
+    /**
+     * Efectúa una búsqueda en el array del juego cuyo titulo se
+     * recibe como parámetro. Es ndiferente mayúsculas y minúsculas
+     * Si existe el juego devuelve su posición, si no existe devuelve -1
+     */
+    public int existeJuego(String titulo) {
+         
+        
+        return 0;
 
-	}
+    }
 
-	/**
-	* Representación textual de la revista
-	* Utiliza StringBuilder como clase de apoyo.
-	* Se incluye el nombre de la  revista on-line.
-	* (Ver resultados de ejecución)
-	*/
-	public String toString() {
-		
-	    
-	    
-		return "";
+    /**
+    * Representación textual de la revista
+    * Utiliza StringBuilder como clase de apoyo.
+    * Se incluye el nombre de la  revista on-line.
+    * (Ver resultados de ejecución)
+    */
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < total; i++)
+        {
+            sb.append(juegos[i].toString()).append("\n");
+        }
+        return sb.toString();
+    }
 
-	}
+    /**
+     *  Se puntúa el juego de título indicado con 
+     *  la puntuación recibida como parámetro. 
+     *  La puntuación es un valor entre 1 y 10 (asumimos esto como correcto)
+     *  Si el juego no existe se muestra un mensaje en pantalla
+     */
+    public void puntuar(String titulo, int puntuacion) {
+         
 
-	/**
-	 *  Se puntúa el juego de título indicado con 
-	 *  la puntuación recibida como parámetro. 
-	 *  La puntuación es un valor entre 1 y 10 (asumimos esto como correcto)
-	 *  Si el juego no existe se muestra un mensaje en pantalla
-	 */
-	public void puntuar(String titulo, int puntuacion) {
-		 
+    }
 
-	}
+    /**
+    * Devuelve un array con los nombres de los juegos 
+    * con una valoración media mayor a la indicada  
+    * 
+    * El array se devuelve todo en mayúsculas y ordenado ascendentemente
+    */
+    public String[] valoracionMayorQue(double valoracion)
+    {
+        int contador = 0;
+        for(int i  = 0; i < total; i++)
+        {
+            if(juegos[i].getValoracionMedia() > valoracion)
+            {
+                contador++;
+            }
+        }
+        
+        String[] aux = new String[total];
+        int j = 0;
+        for(int i = 0; i < total; i++)
+        {
+            if(juegos[i].getValoracionMedia() > valoracion)
+            {
+                aux[j] = juegos[i].getTitulo();
+                j++;
+            }
+        }
+        return aux;
+    }
 
-	/**
-	* Devuelve un array con los nombres de los juegos 
-	* con una valoración media mayor a la indicada  
-	* 
-	* El array se devuelve todo en mayúsculas y ordenado ascendentemente
-	*/
-	public String[] valoracionMayorQue(double valoracion) {
-		 
-	    
-	    
-		return null;
+    /**
+     * Borrar los juegos del género indicado devolviendo
+     * el nº de juegos borradas
+     */
+    public int borrarDeGenero(Genero genero) {
+        
+        
+        return 0;
 
-	}
+    }
 
-	/**
-	 * Borrar los juegos del género indicado devolviendo
-	 * el nº de juegos borradas
-	 */
-	public int borrarDeGenero(Genero genero) {
-		
-	    
-		return 0;
+    /**
+    * Lee de un fichero de texto los datos de los juegos
+    * con ayuda de un objeto de la  clase Scanner
+    * y los guarda en el array. 
+    */
+    public void leerDeFichero() {
+        Scanner sc = null;
+        try {
+            sc = new Scanner(new File("juegos.txt"));
+            while (sc.hasNextLine()) {
+                Juego juego = new Juego(sc.nextLine());
+                this.add(juego);
 
-	}
+            }
 
-	/**
-	* Lee de un fichero de texto los datos de los juegos
-	* con ayuda de un objeto de la  clase Scanner
-	* y los guarda en el array. 
-	*/
-	public void leerDeFichero() {
-		Scanner sc = null;
-		try {
-			sc = new Scanner(new File("juegos.txt"));
-			while (sc.hasNextLine()) {
-				Juego juego = new Juego(sc.nextLine());
-				this.add(juego);
+        } catch (IOException e) {
+            System.out.println("Error al leer del fichero");
+        } finally {
+            sc.close();
+        }
 
-			}
-
-		} catch (IOException e) {
-			System.out.println("Error al leer del fichero");
-		} finally {
-			sc.close();
-		}
-
-	}
+    }
 
 }
